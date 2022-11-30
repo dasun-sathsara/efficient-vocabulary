0f89a24249ce2971fcb8f6693ce1b44906f20c23
Dasun Sathsara <me@dasunsathsara.com>
2022-11-30 19:40:09 +0530

    Update files

---
+++ notebook/test.py
@@ -0,0 +1,5 @@
+from nltk.tokenize import word_tokenize
+
+s = word_tokenize(
+    "Tokenizers divide strings into lists of substrings. For example, tokenizers can be used to find the words and punctuation in a string:."
+)
