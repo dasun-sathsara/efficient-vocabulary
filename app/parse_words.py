c42c721d10f3161e902976128e0c3769ef0061a4
Dasun Sathsara <me@dasunsathsara.com>
2022-12-04 21:55:46 +0530

    Update get_words function to return ordered list of words when needed

--- app/parse_words.py
+++ app/parse_words.py
@@ -4,6 +4,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
 from pathlib import Path
+from parse_srt import parse_subtitle
 
 
 def download_nltk_data() -> None:
@@ -16,19 +17,27 @@ def download_nltk_data() -> None:
     nltk.download("averaged_perceptron_tagger")
 
 
-def get_words_nltk(words: list[str]) -> list[str]:
+def _get_words_nltk(words: list[str], ordered: bool = False, unique: bool = False) -> list[str]:
     """Extract a word list from the sentences and filter out stop words.
     Returns the lemmatized version of the word using parts of speech tagging.
+    By default, for increased performance, returns a unordered list of words with no duplicates.
 
     Args:
         words (list[str]): Subtitle text as a list of strings
+        ordered bool: If True, returns a ordered list of words with duplicates
     """
 
     final_words = []
+    lemmatized_words = []
     lemmatizer = WordNetLemmatizer()
 
     full_string = "\n".join(words)
     words = word_tokenize(full_string)
+
+    # * basic cleaning
+    map(lambda x: x.lower().replace(".", "").replace("?", ""), words)
+    words = list(filter(lambda x: x.isalpha(), words))
+
     tagged_words = nltk.pos_tag(words)
 
     for word_tag in tagged_words:
@@ -49,18 +58,27 @@ def get_words_nltk(words: list[str]) -> list[str]:
         else:
             lemmatized_word = word
 
-        # print(f"{word} ({tag}) -> {lemmatized_word}")
-        final_words.append(lemmatized_word)
+        print(f"{word} ({tag}) -> {lemmatized_word}")
+        lemmatized_words.append(lemmatized_word)
 
-    filtered_words = set(map(lambda x: x.lower().replace(".", "").replace("?", ""), final_words)).difference(
-        set(stopwords.words("english"))
-    )
+    if ordered:
+        for lemmatized_word in lemmatized_words:
+            if not lemmatized_word in stopwords.words("english"):
+                final_words.append(lemmatized_word)
+    else:
+        final_words = list(set(lemmatized_words).difference(set(stopwords.words("english"))))
 
-    return list(filtered_words)
+    return final_words
 
 
-def get_words(words: list[str]) -> list[str]:
-    """Checks if the identified words are in the `all_words` file and filter out words that are in the `common_words` file"""
+def get_words(subtitle_file: Path(), ordered: bool = False) -> list[str]:
+    """Checks if the identified words are in the `all_words` file and filter out words that are in the `common_words` file.
+     By default, for increased performance, returns an unordered a list of words with no duplicates.
+
+    Args:
+        subtitle_file (Path): Subtitle file
+        ordered (bool, optional): If True, returns a ordered list with no duplicates. Defaults to False.
+    """
 
     # * paths
     parent_path = Path(".").parent
@@ -68,16 +86,38 @@ def get_words(words: list[str]) -> list[str]:
     # all_words_path = parent_path / "word_data" / "all_words.txt"
     common_words_path = parent_path / "word_data" / "common_words.txt"
 
-    all_words = set(all_words_path.read_text().split("\n"))
-    common_words = set(common_words_path.read_text().split("\n"))
+    all_words = all_words_path.read_text().split("\n")
+    common_words = common_words_path.read_text().split("\n")
 
-    # * main filtering
-    final_filtered_words = list(set(words).intersection(all_words).difference(common_words))
+    if ordered:
+        word_list = parse_subtitle(subtitle_file)
+        lemmatized_words = _get_words_nltk(word_list, ordered=True)
+        final_filtered_words = []
+
+        # * main filtering
+        for lemmatized_word in lemmatized_words:
+            if (
+                (lemmatized_word in all_words)
+                and (not lemmatized_word in common_words)
+                and (not lemmatized_word in final_filtered_words)
+            ):
+                final_filtered_words.append(lemmatized_word)
+
+    else:
+        word_list = parse_subtitle(subtitle_file)
+        lemmatized_words = _get_words_nltk(word_list)
+
+        # * main filtering
+        final_filtered_words = list(set(lemmatized_words).intersection(set(all_words)).difference(set(common_words)))
 
     # * filtering out words that are shorter than 3 letters
     final_filtered_words = filter(lambda x: len(x) > 2, final_filtered_words)
 
     return list(final_filtered_words)
+
+
+def get_words_with_frequency(words: list[str], sorted: bool = True):
+    ...
 
 
 if __name__ == "__main__":
@@ -86,14 +126,22 @@ if __name__ == "__main__":
     parent_path = Path(".").parent
     subtitle = parent_path / "subtitles" / "sample_subtitle.srt"
 
-    # a = get_words(get_words_nltk())
-    subtitle_list = parse_srt.parse_subtitle(subtitle)
-    nltk_words = get_words_nltk(subtitle_list)
-    final_filtered = get_words(nltk_words)
+    a = get_words(subtitle, ordered=True)
 
-    print(len(final_filtered))
-    for j in final_filtered:
-        print(j)
+    for i in range(len(a)):
+        print(a[i])
+
+        if i == 100:
+            break
+
+    # # a = get_words(get_words_nltk())
+    # subtitle_list = parse_srt.parse_subtitle(subtitle)
+    # nltk_words = _get_words_nltk(subtitle_list)
+    # final_filtered = get_words(nltk_words)
+
+    # print(len(final_filtered))
+    # for j in final_filtered:
+    #     print(j)
 
     # print(len(a))
     # print(a)
--- notebook/typer_test.py
+++ notebook/typer_test.py
@@ -1,15 +1,24 @@
 import typer
+from random import choice
+from typing import Optional
 
 
-def main(input_file: str, output_file: str):
-    """Copies the content of input_file to output_file"""
-    if not input_file.endswith("txt"):
-        print("Input file should be of type txt")
-        raise typer.Abort(1)
+def get_greeting() -> str:
+    return choice(["Hello", "Hola", "Howdy"])
 
-    with open(output_file, "w", encoding="utf-8") as of:
-        with open(input_file, "r", encoding="utf-8") as inputf:
-            of.write(inputf.read())
+
+# * optional CLI argument
+def main(
+    first_name: str = typer.Argument(..., help="first name of the user", metavar="First Name"),
+    last_name: str = typer.Argument("", help="last name of the user", metavar="[Last Name]", rich_help_panel="Optional Arguments"),
+    greeting: str = typer.Argument(
+        get_greeting,
+        help="random greeting will be selected if not set",
+        show_default="random greeting",
+        rich_help_panel="Optional Arguments",
+    ),
+):
+    print(f"{greeting}, {first_name} {last_name}!")
 
 
 if __name__ == "__main__":
